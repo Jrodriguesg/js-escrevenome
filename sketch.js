a62b5558e3f5b170bbb3f721f71df344bc003e15
Jrodriguesg <00001132972796sp@al.educacao.sp.gov.br>
2024-05-27 18:05:27 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("orange");
+  }
+  
+  function draw() {
+    stroke("black");
+    fill("purple");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+    
+  }
+  
